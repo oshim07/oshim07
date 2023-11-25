91d9373fafb24170e248ab89c339d482342b1118
unknown <oshim.gupta@gmail.com>
2023-11-25 21:33:16 +0530

    Saved file as of now

---
+++ ex4.py
@@ -0,0 +1,16 @@
+my_name = "Oshim Gupta"
+my_age = 28 #Not a lie
+my_height = 70 #inches
+my_weight = 255 #lbs
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy. ")
+print(f"He's got {my_eyes} eyes and {my_hair} hairs.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total =my_age+my_height+my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
